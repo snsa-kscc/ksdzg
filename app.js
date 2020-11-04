2dd3d86fb589fd27b59dbea3ec08af0bda5d7f6c
Sinisa Koscec <snsa_kscc@asdf.local>
2020-11-04 12:15:22 +0100

    intro anima

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+gsap.from('.svg__line', {duration: 2, opacity: 0, delay:1, stagger: 1.3})
+gsap.to('.loader', {delay: 4, duration:2, top: '-110%', ease: Expo.easeInOut, onComplete: function e(){
+  document.querySelector('.loader').style.display = 'none';
+}})
+
+// kontaktna
+
 let testForm = document.querySelector(".sixth__form form");
 
 testForm.addEventListener("submit", (e) => {

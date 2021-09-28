1f5bb094721ec72681c7a1d0ecdbf5dc4bafa6ea
Sinisa Koscec <snsa_kscc@asdf.local>
2021-09-28 03:18:40 +0200

    pristup s01e01

--- app-pristup.js
+++ app-pristup.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(CSSRulePlugin);
 gsap.registerPlugin(ScrollTrigger);
 
 const locoScroll = new LocomotiveScroll({
@@ -26,97 +27,28 @@ ScrollTrigger.scrollerProxy("[data-scroll-container]", {
     : "fixed",
 });
 
-//
+const rule = CSSRulePlugin.getRule(".wrapper::after");
 
-let characters = [
-  document.querySelectorAll(".skill__overflow:nth-child(1) .title__text"),
-  document.querySelectorAll(".skill__overflow:nth-child(2) .title__text"),
-  document.querySelectorAll(".skill__overflow:nth-child(3) .title__text"),
-];
-
-characters.forEach((e, index) => {
-  gsap.to(e, 0, { y: 160 });
-});
-
-gsap.from(".svg__line--black", {
-  duration: 2,
-  opacity: 0,
-  delay: 1,
-  stagger: 1.3,
-});
-gsap.to(".loader", {
-  delay: 4,
-  duration: 1.7,
-  top: "-100%",
-  ease: Expo.easeInOut,
-  onComplete: () => {
-    document.querySelector("html").classList.add("overflow--reset");
-    document.querySelector("body").classList.add("overflow--reset");
-    animateUpCharacter();
+gsap.to(rule, {
+  scrollTrigger: {
+    trigger: ".test",
+    scroller: "[data-scroll-container]",
+    start: "top 20%",
   },
+  cssRule: {
+    scaleY: 0,
+  },
+  duration: 1,
 });
-
-function animateUpCharacter() {
-  characters.forEach((character) => {
-    character.forEach((element, index) => {
-      gsap.to(element, 0.3, {
-        y: 0,
-        delay: 0.03 * index,
-        ease: "power1.out",
-      });
-    });
-  });
-}
-
-// function animateUpCharacter() {
-//   characters[0].forEach((e, index) => {
-//     // gsap.to(e, 0, { y: 105 });
-//     gsap.to(e, 0.3, {
-//       y: 0,
-//       delay: 0.03 * index,
-//       ease: "power1.out",
-//     });
-//   });
-//   characters[1].forEach((e, index) => {
-//     // gsap.to(e, 0, { y: 105 });
-//     gsap.to(e, 0.4, {
-//       y: 0,
-//       delay: 0.04 * index,
-//       ease: "power1.out",
-//     });
-//   });
-//   characters[2].forEach((e, index) => {
-//     // gsap.to(e, 0, { y: 105 });
-//     gsap.to(e, 0.4, {
-//       y: 0,
-//       delay: 0.04 * index,
-//       ease: "power1.out",
-//     });
-//   });
-// }
-
-// gsap.set(".logotip__ksd", { svgOrigin: "842 595" });
-let tl = gsap.to(".logotip__ksd", { scale: 0.8, opacity: 1 });
 
 ScrollTrigger.create({
-  trigger: ".fourth",
-  start: "50% 50%",
-  end: () => "+=" + document.querySelector(".fourth").offsetWidth,
+  trigger: ".test",
   scroller: "[data-scroll-container]",
-  animation: tl,
-  scrub: true,
-  pin: true,
+  start: "top 20%",
+  onEnter: () => {
+    group.classList.add("show");
+  },
 });
-
-// gsap.to(".fade-in", {
-//   scrollTrigger: {
-//     trigger: ".fade-in",
-//     scroller: "[data-scroll-container]",
-//     start: "1px 85%",
-//   },
-//   opacity: 1,
-//   duration: 1,
-// });
 
 const observableItems = document.querySelectorAll(".fade");
 const options = {
@@ -141,47 +73,11 @@ observableItems.forEach((item) => {
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 ScrollTrigger.refresh();
 
-//
-
 // skrol do vrha
 
 window.addEventListener("unload", () => {
   window.scrollTo(0, 0);
 });
-
-// kontaktna
-
-let testForm = document.querySelector(".form form");
-
-testForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const formData = new FormData(testForm);
-  fetch(testForm.getAttribute("action"), {
-    method: "POST",
-    headers: {
-      Accept: "application/x-www-form-urlencoded;charset=UTF-8",
-      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-    },
-    body: new URLSearchParams(formData).toString(),
-  }).then((res) => {
-    if (res) {
-      document.querySelector(".confirmation").classList.add("show");
-      testForm.reset();
-      setTimeout(() => {
-        document.querySelector(".confirmation").classList.remove("show");
-      }, 5000);
-    }
-  });
-});
-
-// const btn = document.querySelector(".testni");
-// btn.addEventListener("click", () => {
-//   document.querySelector(".confirmation").classList.add("show");
-//   setTimeout(() => {
-//     document.querySelector(".confirmation").classList.remove("show");
-//   }, 5000);
-// });
 
 // funkcionalnost za top za navbar
 
